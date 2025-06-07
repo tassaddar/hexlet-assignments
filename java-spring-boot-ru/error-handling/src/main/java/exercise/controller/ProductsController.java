0ef402c8a36c0eb4f58a006d089fabad309299f8
@hexlet/cli <support@hexlet.io>
2025-06-07 11:50:52 +0400

    submit java-spring-boot-ru/error-handling

---
+++ java-spring-boot-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,64 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(path = "")
+    public List<Product> index() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    // BEGIN
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable long id, @RequestBody Product product) {
+        var productFromDB = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        productFromDB.setPrice(product.getPrice());
+        productFromDB.setTitle(product.getTitle());
+
+        productRepository.save(productFromDB);
+        return product;
+    }
+    // END
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable long id) {
+        productRepository.deleteById(id);
+    }
+}
