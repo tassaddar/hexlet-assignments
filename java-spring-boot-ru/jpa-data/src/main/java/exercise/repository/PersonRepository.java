a97f2d2bf50bd7847d466def44e7a24ba17c8591
@hexlet/cli <support@hexlet.io>
2025-06-07 11:14:43 +0400

    submit java-spring-boot-ru/jpa-data

---
+++ java-spring-boot-ru/jpa-data/src/main/java/exercise/repository/PersonRepository.java
@@ -0,0 +1,14 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import exercise.model.Person;
+
+import java.util.List;
+
+// BEGIN
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
+// END
